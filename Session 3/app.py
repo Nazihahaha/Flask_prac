85d3dc10de1de5fa97e17a640fc7f0d58dd1a6d0
Nazihah Islam <140833772+Nazihahaha@users.noreply.github.com>
2025-05-10 22:55:47 +0600

    placeholder using jinja

--- Session 3/app.py
+++ Session 3/app.py
@@ -6,8 +6,9 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="Home")
 #invoking these hmtl files
+#passing page name dynamically
 
 @app.route("/about")
 def about():
