599ff010ad2b8791b5a1617233ebc3579378599e
Nazihah Islam <140833772+Nazihahaha@users.noreply.github.com>
2025-05-13 16:14:17 +0600

    wt forms

--- Session 4/forms.py
+++ Session 4/forms.py
@@ -1,7 +1,22 @@
 from flask_wtf import FlaskForm
+from wtforms import (StringField, SelectField, DateField, PasswordField, SubmitField)
+from wtforms.validators import (DataRequired, Length, Email, Optional, EqualTo)
 
 class SignupForm(FlaskForm):
-    pass
+    username = StringField( "Username" , validators=[DataRequired(), Length( 3, 30)])
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+
+    gender = SelectField("Gender", choices=["Male", "Female", "Other"], validators=Optional())
+
+    dob = DateField("Date of Birth", validators=[Optional()])
+
+    password = PasswordField("Password", validators=[DataRequired(), Length(5,25)])
+
+    conf_password = PasswordField("Confirm Password", validators=[DataRequired(), Length(5,25), EqualTo("password")])
+
+    submit = SubmitField("Sign Up")
+
 
 class LoginForm(FlaskForm):
     pass
