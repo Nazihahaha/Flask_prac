b51b927a12325220f8f91907ba98c128923bb7b5
Nazihah Islam <140833772+Nazihahaha@users.noreply.github.com>
2025-07-18 00:06:54 +0600

    Fix typo in Employee model and add __repr__ method
    Corrected 'Inetger' to 'Integer' in the Employee model's age column and implemented the __repr__ method for better object representation. Added compiled Python file and database file.

--- Session 5 - Databases part 1/db_crud_demo.py
+++ Session 5 - Databases part 1/db_crud_demo.py
@@ -10,11 +10,11 @@ db = SQLAlchemy(app)
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), nullable=False )
-    age = db.Column(db.Inetger, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
 
     def __repr__(self):
-        pass
+        return f"Employee( '{self.name}', {self.age} , '{self.email}' )"
 
 
 
